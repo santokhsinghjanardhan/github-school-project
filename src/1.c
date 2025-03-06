190ef236246c28e85a7d98f5314120cb9d3833dc
santokhsinghjanardhan <santokhsinghjanardhan@gmail.com>
2025-03-06 13:48:56 +0000

    Add 1.c

---
+++ src/1.c
@@ -0,0 +1,10 @@
+int main() {
+    int a = 5;
+    int b = 10;
+    if (a > b) {
+        printf("a is greater than b\n");
+    } else {
+        printf("b is greater than a\n");
+    }
+    return 0;
+}
