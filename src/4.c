1953ff7e80834d6f45ed54c3e6c6ce56e91c31aa
santokhsinghjanardhan <santokhsinghjanardhan@gmail.com>
2025-03-09 03:46:34 +0000

    Add 4.c

---
+++ src/4.c
@@ -0,0 +1,12 @@
+
+int main() {
+    int x = 5;
+    int y = 10;
+    if(x > y) {
+        printf("X is greater than Y\n");
+    } else if (x < y) {
+        printf("Y is greater than X\n");
+    } else {
+        printf("X and Y are equal\n");
+    }
+}
