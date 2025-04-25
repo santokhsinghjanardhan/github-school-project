cb64587caae7836d09b10ab3433671d1cea79a1b
santokhsinghjanardhan <santokhsinghjanardhan@gmail.com>
2025-04-25 07:25:24 +0000

    Add 32.c

---
+++ src/32.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 5;
+    int num2 = 3;
+    int sum;
+
+    sum = num1 + num2;
+    printf("The sum of %d and %d is: %d\n", num1, num2, sum);
+
+    return 0;
+}
